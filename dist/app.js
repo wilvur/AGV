5404c1d874304bd2854fbb5f1bf0e6c89671419a
Javier <wilvur@hotmail.com>
2021-11-10 20:31:01 -0300

    ver con intectividad en busqueda

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const serviciosSec = document.getElementById("#serviciosSec");
     const busquedasSec = document.getElementById("#busquedasSec");
     const contactoSec = document.getElementById("#contactoSec");
-    const detailModal = document.getElementById("modal");
  
     // LISTENER
 
@@ -20,47 +19,96 @@ window.addEventListener('DOMContentLoaded', () => {
     )
     // CARGAS INICIALES 
     cargarBusquedas();
-    cargaCategorias();
-    
+    cargaCategorias();   
 }
-)
+);
 
-;
-
-// cargar de moda 
-function detail() {
-    aviso = Armarcard("aviso.titulo", "aviso.introtext", "aviso.activo", "aviso.descripcion", "aviso.categoriaDesc", "aviso.zonaDesc","aviso.fechaAlta", true)
-    busquedasContainer.innerHTML = aviso;
-}
-
+var ultimaCat = ""
+// para restablecer todas las categoraias
 function restoreBusquedas(){
-    cargaCategorias();
-    
-}
+    if (ultimaCat === ''){
+        cargarBusquedas(); 
+    }else{
+        cargarBusquedas(ultimaCat);
+    } 
 
+}
 
 // PREPARACION DE DATOS DATOS
-function cargarBusquedas(id) {
+function cargarBusquedas(categoriaId) {
+    busquedasContainer.classList.add("md:grid-cols-2", "lg:grid-cols-3", "md:gap-5");
     //carga las busquedas segun las categorias
     //si no tengo categorias cargo todo    
-    if (id === undefined){
+    if (categoriaId === undefined){
     const busquedas = avisos.filter(aviso => aviso.activo === "1").map(aviso => {
-        return Armarcard(aviso.titulo, aviso.introtext, aviso.activo, aviso.descripcion, aviso.categoriaDesc, aviso.zonaDesc, aviso.fechaAlta, false)  
+        return Armarcard(aviso.titulo, aviso.introtext, aviso.activo, aviso.descripcion, aviso.categoriaDesc, aviso.zonaDesc, aviso.fechaAlta, aviso.id)  
     }).sort(aviso => aviso.fechaAlta).join('');
     busquedasContainer.innerHTML = busquedas;
     //cargo por categoria
     } else {
-     const idCategoria = id.toString();
-     const busquedasF = avisos.filter(aviso => aviso.categoria === idCategoria ).map(aviso => {
-            return Armarcard(aviso.titulo, aviso.introtext, aviso.activo, aviso.descripcion, aviso.categoriaDesc, aviso.zonaDesc,aviso.fechaAlta, false)  
+     const categoria = categoriaId.toString();
+     const busquedasF = avisos.filter(aviso => aviso.categoria === categoria ).map(aviso => {
+            return Armarcard(aviso.titulo, aviso.introtext, aviso.activo, aviso.descripcion, aviso.categoriaDesc, aviso.zonaDesc,aviso.fechaAlta, aviso.id)  
         }).sort(aviso => aviso.fechaAlta).join('');
         categoriaMenu.open = false;
+        ultimaCat = categoria;
+        
     
-    const categoriaDescripcion = categorias.filter(categoria => categoria.id === idCategoria).map(categoria => {return categoria.descripcion});
+    const categoriaDescripcion = categorias.filter(categoria => categoria.id === categoriaId.toString()).map(categoria => {return categoria.descripcion});
     categoriasSummary.innerHTML = categoriaDescripcion;
     busquedasContainer.innerHTML = busquedasF;
-
     }
+}
+
+function cargarBusqueda(id) {
+    // remuevo la clases para acomodar
+    busquedasContainer.classList.remove("md:grid-cols-2", "lg:grid-cols-3", "md:gap-5");
+    dropMenu.classList.toggle('hidden');
+    scroleaTo('busquedasSec');
+
+    const busquedasF = avisos.filter(aviso => aviso.id === id.toString() ).map(aviso => {
+            return  `
+    <div class="w-full  py-1 sm:p-3 text-white bg-opacity-70 cursor-pointer">
+    <!-- boton cerrar -->
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" viewBox="0 0 20 20" fill="currentColor" onClick="restoreBusquedas()" >
+<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
+</svg>
+</div>
+ <article id="busquedaD" class="bg-white w-full md:w-1/2 rounded-lg shadow-lg mx-auto p-3 mb-12 ">                  
+    <span class="text-3xl font-bold text-blue-500 mb-3">${aviso.titulo}</span>
+    <div class="text-sm my-3">
+        ${aviso.introtext}
+    </div>
+     <span class="font-bold" >Rubro:</span> ${aviso.categoriaDesc} - <span class="font-bold">Zona:</span> ${aviso.zonaDesc} 
+   
+     <div class="mt-3">${aviso.descripcion}</div>
+     <div class="flex shadow-lg rounded-full ring-2 p-1 text-center justify-center my-2 hover:bg-azul_Agv1 hover:text-white">             
+        <a href="mailto:agvconsultorarrhh@gmail.com <agvconsultorarrhh@gmail.com>;?&subject=Postulacion-ref:${aviso.id}]&:cc=&bcc=&body=http://www.agv-recursoshumanos.com.ar/">
+        Postularse</a> 
+     </div>
+
+    <div class="flex justify-between ">
+    <div class="text-xs text-gray-500">
+        <div><span class="font-bold">Publicado:</span> ${aviso.fechaAlta}</div>
+    </div>
+    <!-- iconos -->
+    <div class="flex  cursor-pointer text-xs relative items-center ">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
+            </svg>
+        Compartir  
+        <div class="bg-white absolute flex justify-between rounded-lg shadow-lg top-6 right-0 w-40 h-16 p-4">
+            <a href="https://www.facebook.com/sharer/sharer.php?u=http://www.agv-recursoshumanos.com.ar/" target="_blank"> <img src="./img/facebook.png" class="w-8 "alt=""> </a>
+            <a href="whatsapp://send?text=http://www.agv-recursoshumanos.com.ar/" data-action="share/whatsapp/share" target="_blank"> <img src="./img/whatsapp.png" class="w-8 " alt=""></a>
+            <a href="https://www.linkedin.com/shareArticle?mini=true&url=http://www.agv-recursoshumanos.com.ar/" target="_blank"> <img src="./img/linkedin.png" class="w-8 "alt=""> </a>
+        </div> 
+    </div>
+</div>
+</article>
+`});
+
+    busquedasContainer.innerHTML = busquedasF;
+    
 }
 
 // CATEGORIAS 
@@ -74,49 +122,25 @@ function cargaCategorias () {
 }
 
 // el template en html
-function Armarcard(titulo, intro, activo, descripcion, categoriaDesc, zona, fechaAlta, detail) {
-    const detalle = detail; 
-   
+function Armarcard(titulo, intro, activo, descripcion, categoriaDesc, zona, fechaAlta, id) { 
     return `
-    <article class="${detalle? "w-full md:w-1/3 mx-auto my-auto": "" } bg-white rounded-lg flex-col shadow-lg text-gray-700 p-3 mb-3 relative">
-     <div class="absolute right-2 -top-2 shadow-lg bg-white rounded-full ring-2 p-1 ">
-     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-   </svg>
-   </svg>
-
-     </div>
-    
-    <span class="text-lg font-bold text-blue-500 mb-3">${titulo}</span>
-    <div id="introtext" class="text-sm">
+    <article class="w-full  bg-white rounded-lg flex-col shadow-lg text-gray-700 p-3 mb-3 relative cursor-pointer" onClick="cargarBusqueda(${id.toString()})">
+         <div class="absolute right-2 -top-2 shadow-lg bg-white rounded-full ring-2 p-1 ">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+        </svg>
+        </svg>
+    </div>
+       <span class="text-lg font-bold text-blue-500 mb-3">${titulo}</span>
+    <div class="text-sm mt-3 sm:h-24 overflow-hidden">
         ${intro}
     </div>
-    <details class="text-sm my-3 ">
-        <summary class="cursor-pointer">
-            <span class="font-bold" >Rubro:</span> ${categoriaDesc} - <span class="font-bold">Zona:</span> ${zona}  <span class="font-bold text-blue-500">- Mas información </span>
-        </summary>
-        <div class="mt-3">${descripcion}</div>
-        
-    </details>
-    <div class="flex justify-between ">
-    <div class="text-xs text-gray-500">
-        <div><span class="font-bold">Publicado:</span> ${fechaAlta}</div>
+    <div class="flex-col justify-items-end align-baseline text-xs mt-3">
+    <span class="font-bold" >Rubro:</span> ${categoriaDesc} - <span class="font-bold">Zona:</span> ${zona}  
+    <div class="text-xs text-gray-500">    
+            <span class="font-bold">Publicado:</span> ${fechaAlta}
     </div>
-    <!-- iconos -->
-    <div class="flex items-end cursor-pointer text-xs relative ">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-6" viewBox="0 0 20 20" fill="currentColor">
-            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-        </svg>
-        Postular
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-        </svg>
-        Compartir
-     
-    </div>
-    ${detalle? "<button type='button' onClick='restoreBusquedas()'>true</button>": "false"}
-</div>
+   </div>
 </article>
 `;
 }
@@ -136,14 +160,3 @@ function scroleaTo(id) {
       dropMenu.classList.toggle('hidden');
 }
 
-/*
-<a href="https://www.facebook.com/sharer/sharer.php?u=https://www.websiteplanet.com">Compartir</a>
-<a href="https://twitter.com/intent/tweet?url=https://www.websiteplanet.com&text=">Compartir</a>
-<a href="https://www.linkedin.com/shareArticle?mini=true&url=https://www.websiteplanet.com">Compartir</a>
-
-<a href="https://www.linkedin.com/shareArticle?mini=true&url=https://www.websiteplanet.com">Compartir</a>
-
-<a href="mailto:info@example.com?&subject=asunto&cc=&bcc=&body=https://www.websiteplanet.com">Compartir</a>
-
-
-*/
